c2845d0395b8c4523a28d7f6afaa080c63780122
McGee <mo.mcgee@gmail.com>
2018-07-29 23:08:15 +0300

    Третий день... комиты падали и падали

--- js/script.js
+++ js/script.js
@@ -1,37 +1,51 @@
-let chooseBtn = document.getElementById('choose'),
-	recieveBtn = document.getElementById('receive'),
-	heading = document.getElementsByTagName('h2')[0],
-	nameInput = document.getElementsByClassName("contactform_name")[0],
-	phoneInput = document.querySelector('.contactform_phone'),
-	mailInput = document.querySelectorAll('.contactform_mail')[0],
-	modal = document.querySelector('.modal'),
-	close = document.querySelector('.close'),
-	text = document.getElementsByName('message')[0];
+window.addEventListener('DOMContentLoaded', function() {
+	let products = document.querySelectorAll('.product'),
+	buttons = document.getElementsByTagName('button'),
+	open = document.getElementsByClassName('open')[0];
+	
+	function createCart() {
+		let cart = document.createElement('div'),
+			field = document.createElement('div'),
+			heading = document.createElement('h2'),
+			close = document.createElement('button');
+			
+		cart.classList.add("cart");
+		field.classList.add("cart-field");
+		close.classList.add("close");
+		
+		close.textContent = "Закрыть";
+		heading.textContent = "В вашей корзине: ";
+		
+		document.body.appendChild(cart);  
+		cart.appendChild(heading);
+		cart.appendChild(field);
+		cart.appendChild(close);
+	}	
+		createCart();
+		
+		let field = document.querySelector(".cart-field"),
+		cart = document.querySelector(".cart"),
+		close = document.querySelector(".close");
 
-function hover() {
-	heading.textContent = "Действительно все!"
-}
-
-function out() {
-	heading.textContent = "Все включено!"
-}
-
-heading.addEventListener("mouseenter", hover);
-heading.addEventListener("mouseleave", out);
-
-recieveBtn.addEventListener('click', function() {
-	modal.style.display = "block"
+		for(let i = 0; i < buttons.length; i++) {
+			buttons[i].addEventListener('click', function () {
+				let item = products[i].cloneNode(true),
+				btn = item.querySelector('button');
+			
+			btn.remove();
+			field.appendChild(item);
+			products[i].remove();			
+			})
+		}
+	
+	function openCart() {
+		cart.style.display = "block"
+	};
+	
+	function closeCart() {
+		cart.style.display = "none"
+	};
+	
+	open.addEventListener('click', openCart);
+	close.addEventListener('click', closeCart);
 });
-
-
-close.addEventListener('click', function() {
-	modal.style.display = "none"
-});
-
-nameInput.addEventListener("input", function() {
-	text.value = "Меня зовут " + nameInput.value + ". И я хочу спросить: ";
-
-	if (nameInput.value == "") {
-		text.value = ""
-	}
-})
